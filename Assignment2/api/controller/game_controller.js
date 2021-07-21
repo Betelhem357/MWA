fee44221d26317a2b3aeee7199a8cd61cf52c177
Betelhem357 <mbetelhem2017@gmail.com>
2021-07-20 20:30:04 -0500

    making route separate

--- Assignment2/api/controller/game_controller.js
+++ Assignment2/api/controller/game_controller.js
@@ -1,5 +1,15 @@
 const games_json_data = require("../../resources/games.json");
-module.exports.getAllGames = (req , res)=>{
-    
-    res.status(200).json(games_json_data);
- };
+module.exports.getAllGames = (req, res) => {
+    // It is not recommended to display all json file so i use to display 10 elements by default
+    let offset = 0;
+    let count = 10;
+    if (req.query && req.query.offset) {
+        offset = parseInt(req.query.offset);
+    }
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count);
+    }
+    let data = games_json_data.slice(offset, offset + count);
+    console.log("It is not recommended to display all json file so i use to display 10 elements by default");
+    res.status(200).json(data);
+};
--- Assignment2/api/route/index.js
+++ Assignment2/api/route/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const game_controller = require("../controller/game_controller.js");
+const add_param_controller = require("../controller/add_parameters.js");
 
 router.route("/").get((req , res)=>{
 
-    console.log("I am Get request :)!");
+    //console.log("I am Get request :)!");
     res.status(200).sendFile("index.html");
  });
 
- //router.route('/add/:first_num' ).get(game_controller.getAllGames);
  router.route('/games' ).get(game_controller.getAllGames);
+
+ router.route('/add/:first_num' ).get(add_param_controller.addParameters);
 
  module.exports = router;
