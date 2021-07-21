b58a947f9edab30b0cbec7573630069cb3e7ae52
Betelhem357 <mbetelhem2017@gmail.com>
2021-07-20 20:36:04 -0500

    modification on game controller

--- Assignment2/api/controller/game_controller.js
+++ Assignment2/api/controller/game_controller.js
@@ -1,6 +1,6 @@
 const games_json_data = require("../../resources/games.json");
 module.exports.getAllGames = (req, res) => {
-    // It is not recommended to display all json file so i use to display 10 elements by default
+    // It is not recommended to display all json file so i use display 10 elements by default
     let offset = 0;
     let count = 10;
     if (req.query && req.query.offset) {
@@ -12,4 +12,6 @@ module.exports.getAllGames = (req, res) => {
     let data = games_json_data.slice(offset, offset + count);
     console.log("It is not recommended to display all json file so i use to display 10 elements by default");
     res.status(200).json(data);
+    //To send all data we can use
+    //res.status(200).json(games_json_data);
 };
