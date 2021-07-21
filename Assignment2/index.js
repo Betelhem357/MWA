5c91e63ce08f2083e284c4dd596c385db0b5ba97
Betelhem357 <mbetelhem2017@gmail.com>
2021-07-20 21:06:05 -0500

    clean up code

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const path = require("path");
+require("dotenv").config();
 const app = express();
+app.set("port",process.env.PORT);
 const router = require("./api/route");
 
 app.use(express.static('public'));
 app.use("/api",router);
-app.set("port",5353);
+
 
 const server = app.listen(app.get("port"),function(){
     console.log("Server connected to port: "+this.address().port);
